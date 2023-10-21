fc19bfd3d05e8da2abed093f4e9517acf5959a51
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-10-21 17:35:49 +0300

    feat: Initialized the backend

---
+++ backend/src/test/java/app/linguistai/linguistai/LinguistaiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.linguistai.linguistai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguistaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
