7482d720c61693880f14a529ab97faa7d30836f4
Ilkim Elif Kervan <elif.kervan.1907@gmail.com>
2023-10-21 19:02:59 +0300

    feat: Added spring security, add register, login, change password

---
+++ backend/src/main/java/app/linguistai/linguistai/response/RRefreshToken.java
@@ -0,0 +1,10 @@
+package app.linguistai.linguistai.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RRefreshToken {
+    private String accessToken;
+}
