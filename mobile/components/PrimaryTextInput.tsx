9b9ce9eeca863ae1b9a33966e28ab1c451282b55
Tolga Özgün <tolgaozgunn@gmail.com>
2023-10-22 16:49:50 +0300

    Update PrimaryTextInput and LoginScreen

--- mobile/components/PrimaryTextInput.tsx
+++ mobile/components/PrimaryTextInput.tsx
@@ -1,25 +1,61 @@
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, TextInputProps, View, Text } from "react-native";
 
 import Colors from "../theme/colors";
+import { useController, useFormContext } from "react-hook-form";
 
-interface PrimaryTextInputProps {
-    placeholder: string;
-    secureTextEntry?: boolean;
+interface PrimaryTextInputProps extends TextInputProps {
+    name: string;
+    label: string;
+    rules: any;
+    defaultValue: string;
 }
 
 const PrimaryTextInput = (props: PrimaryTextInputProps) => {
 
+    const { name } = props;
+    
+    const formContext = useFormContext();
+    
+    if (!formContext || !name) {
+      const msg = !formContext ? "TextInput must be wrapped by the FormProvider" : "Name must be defined"
+        console.error(msg)
+      return null
+    }
+    
+    return <ControlledInput {...props} />;
+    
+};
+
+const ControlledInput = (props: PrimaryTextInputProps) => {
+
+
+    const { field } = useController({name: props.name, rules: props.rules, defaultValue: props.defaultValue});
 
     return (
-        <TextInput
-            style={styles.input}
-            placeholder={props.placeholder}
-            secureTextEntry={props.secureTextEntry}
-        />
+        <View style={styles.container}>
+            {props.label && (<Text style={styles.label}>{props.label}</Text>)}
+              <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.input}
+                onChangeText={field.onChange}
+                onBlur={field.onBlur}
+                value={field.value}
+                {...props}
+              />
+            </View>
+          </View>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+
+    },
+    label: {
+
+    },
+    inputContainer: {
+    },  
     input: {
       height: 60,
       margin: 12,
