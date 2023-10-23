bef61251a7768f970abf97c70644fa154cba8dd1
Tolga Özgün <tolgaozgunn@gmail.com>
2023-10-23 11:33:37 +0300

    Add icons to bottom navigation, update naming between navigations

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,6 +3,7 @@ import LandingScreen from "./screens/LandingScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginScreen from "./screens/LoginScreen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomNavigation from "./components/navigation/BottomNavigation";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -16,6 +17,11 @@ export default function App() {
           options={{ headerShown: false }}
         />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Main"
+          component={BottomNavigation}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- mobile/components/navigation/BottomNavigation.tsx
+++ mobile/components/navigation/BottomNavigation.tsx
@@ -1,22 +1,61 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import LandingScreen from "../../screens/LandingScreen";
 import HomeScreen from "../../screens/HomeScreen";
 import ChatScreen from "../../screens/ChatScreen";
 import LeaderboardScreen from "../../screens/LeaderboardScreen";
 import ProfileScreen from "../../screens/ProfileScreen";
-import { Settings } from "react-native";
 import SettingsScreen from "../../screens/SettingsScreen";
+import IonIcons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigation = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IonIcons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IonIcons name="chatbox-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Leaderboard"
+        component={LeaderboardScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IonIcons name="podium-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IonIcons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <IonIcons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
