3037fc0a5e4219344a179f42337a2eca7b1f2613
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2023-10-28 12:27:51 +0300

    test(LINGUIST-66): Added 3 more unit tests

--- backend/src/test/java/app/linguistai/bmvp/service/gamification/UserStreakServiceTest.java
+++ backend/src/test/java/app/linguistai/bmvp/service/gamification/UserStreakServiceTest.java
@@ -1,11 +1,14 @@
 package app.linguistai.bmvp.service.gamification;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.DisplayName;
@@ -13,8 +16,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import app.linguistai.bmvp.exception.NotFoundException;
 import app.linguistai.bmvp.model.User;
+import app.linguistai.bmvp.model.UserStreak;
 import app.linguistai.bmvp.repository.gamification.IUserStreakRepository;
+import app.linguistai.bmvp.security.JWTUtils;
 
 @SpringBootTest
 public class UserStreakServiceTest {
@@ -24,9 +30,12 @@ public class UserStreakServiceTest {
     @Mock
     private IUserStreakRepository userStreakRepository;
 
+    @Mock
+    private JWTUtils jwtUtils;
+
     @DisplayName("When given a valid new User, create the relevant UserStreak")
     @Test
-    void WhenValidNewUserThenCreateUserStreak() {
+    void whenValidNewUserThenCreateUserStreak() {
         try {
             User user = new User();
             user.setEmail("test@test.test");
@@ -38,6 +47,43 @@ public class UserStreakServiceTest {
 
             assertEquals(true, userStreakService.createUserStreak(user));
             verify(userStreakRepository, times(1)).save(any());
+        }
+        catch (Exception e) {
+            fail("TEST FAILED: " + e.getMessage());
+        }
+    }
+
+    @DisplayName("When updating UserStreak for a user with no existing streak, throw a NotFoundException")
+    @Test
+    void whenUserHasNoUserStreakForUpdateUserStreakThenThrowNotFoundException() {
+        String email = "test@test.test";
+
+        when(userStreakRepository.findByUserEmail(email)).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userStreakService.updateUserStreak(email));
+    }
+
+    @DisplayName("When given an null input while fetching UserStreak, throw an exception")
+    @Test
+    void whenGivenNullInputForGetUserStreakThenThrowException() {
+        try {
+            String input = null;
+            assertThrows(Exception.class, () -> userStreakService.getUserStreak(input));
+        }
+        catch (Exception e) {
+            fail("TEST FAILED: " + e.getMessage());
+        }
+    }
+
+    @DisplayName("When all UserStreaks are fetched, then return all UserStreaks successfully")
+    @Test
+    void whenFetchedAllUserStreaksThenReturnAllUserStreaks() {
+        try {
+            List<UserStreak> userStreaks = Arrays.asList(new UserStreak(), new UserStreak());
+
+            when(userStreakRepository.findAll()).thenReturn(userStreaks);
+
+            assertEquals(userStreaks.size(), userStreakService.getAllUserStreaks().size());
         }
         catch (Exception e) {
             fail("TEST FAILED: " + e.getMessage());
