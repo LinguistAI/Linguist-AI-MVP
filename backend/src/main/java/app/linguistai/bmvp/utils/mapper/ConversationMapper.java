ccb2ec2c8504b9800ca8753077660cf220cddb74
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2023-10-31 23:26:31 +0300

    fix(LINGUIST-94): Removed unnecessary fields from RConversation like user passwords, also sorted returned messages in the list so first element is the most recent message (#37)

--- backend/src/main/java/app/linguistai/bmvp/utils/mapper/ConversationMapper.java
+++ backend/src/main/java/app/linguistai/bmvp/utils/mapper/ConversationMapper.java
@@ -1,12 +1,13 @@
 package app.linguistai.bmvp.utils.mapper;
 
 import app.linguistai.bmvp.model.Conversation;
-import app.linguistai.bmvp.model.Message;
 import app.linguistai.bmvp.response.RConversation;
+import app.linguistai.bmvp.response.RMessage;
+
 import java.util.List;
 
 public class ConversationMapper {
-    public static RConversation toRConversation(Conversation conversation, Boolean isNewConversation, List<Message> messages) {
+    public static RConversation toRConversation(Conversation conversation, Boolean isNewConversation, List<RMessage> messages) {
         return new RConversation(
             conversation.getConversationId(),
             conversation.getUser().getUsername(),
--- backend/src/main/java/app/linguistai/bmvp/utils/mapper/MessageMapper.java
+++ backend/src/main/java/app/linguistai/bmvp/utils/mapper/MessageMapper.java
@@ -4,6 +4,9 @@ import app.linguistai.bmvp.model.Message;
 import app.linguistai.bmvp.response.RMessage;
 import app.linguistai.bmvp.response.RMessagePair;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageMapper {
     public static RMessage toRMessage(Message message) {
         return new RMessage(
@@ -18,5 +21,15 @@ public class MessageMapper {
 
     public static RMessagePair toRMessagePair(RMessage userMessage, RMessage replyMessage) {
         return new RMessagePair(userMessage, replyMessage);
+    }
+
+    public static List<RMessage> toRMessageList(List<Message> messageList) {
+        List<RMessage> rMessageList = new ArrayList<>();
+
+        for (Message message : messageList) {
+            rMessageList.add(toRMessage(message));
+        }
+
+        return rMessageList;
     }
 }
